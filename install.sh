a3172a5f1604fe7b421e65723154009cf3e59526
Mark Bann <102082177+markbann@users.noreply.github.com>
2024-10-07 13:30:03 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+git config --global alias.br branch
+git config --global alias.co checkout
+git config --global alias.cob "checkout -b"
+git config --global alias.com "checkout main"
+git config --global alias.mm "merge main"
+git config --global alias.rbm "rebase main"
+git config --global alias.st status
+
+git config --global push.autosetupremote checkout
